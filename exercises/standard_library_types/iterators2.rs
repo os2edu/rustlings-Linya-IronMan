4feebafd37e7986cd344d4efe3adc37a7e7053e6
林崖 <cdthanye32@163.com>
2022-11-20 09:55:17 +0800

    cache: iterators3

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,16 +3,21 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
+
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            format!(
+                "{}{}",
+                first.to_uppercase().collect::<String>(),
+                &c.as_str() // ello
+            )
+        }
     }
 }
 
@@ -21,7 +26,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // 数组是不能直接 遍历的，需要通过 iter() 生成一个 iterator
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -29,7 +35,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    capitalize_words_vector(words).join("")
 }
 
 #[cfg(test)]
